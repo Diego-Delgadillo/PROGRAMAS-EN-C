c7769f317bb705603458710266124674d3f0e7d0
Diego Ricardo Delgadillo Gómez <120683746+Diego-Delgadillo@users.noreply.github.com>
2023-01-24 10:28:37 -0600

    V1.0

---
+++ Operador de asignacion.c
@@ -0,0 +1,19 @@
+//Operador de asignacion
+
+#include<stdio.h>
+
+int main(){// Funcion principal
+
+	int a,b,c; // Declaramos las variables
+	a =10;
+    //a+= 10;	// Esto es lo mismo que a = a+10
+	//a -=5; //a = a-5
+	//a *=2; //a = a*2
+	a /= 2; // a = a / 2
+	
+	
+	printf("El valor de a es: %i",a);
+	
+	return 0;
+}
+
